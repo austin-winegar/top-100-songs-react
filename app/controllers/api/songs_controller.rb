551c28bd919e2c5ae40bc1a4475309639bb5ba62
Austin Winegar <atwinegar@gmail.com>
2017-12-07 19:49:04 -0700

    fresh song list

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,13 +1,33 @@
 class Api::SongsController < ApplicationController
   def index
+    render json: Song.all
   end
 
   def create
+    song = Song.new(song_params)
+    if song.save
+      render json: song
+    else
+      render json: { message: 'song failed to create'}, status: 422
+    end
   end
 
   def update
+    song = Song.find(params[:id])
+    if song.update(song_params)
+      render json: song
+    else
+      render json: { message: 'song failed to update' }, status: 422
+    end
   end
 
   def destroy
+    Song.find(params[:id]).destroy
+    render json: { message: 'song has been deleted' }
   end
+
+  private 
+    def song_params
+      params.require(:song).permit(:name, :artist)
+    end
 end
